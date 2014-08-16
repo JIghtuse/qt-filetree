aa97e76da11d8318266912bcd5f1ab2d81f7d3b9
Boris Egorov <jightuse@gmail.com>
2014-08-16 22:31:45 +0700

    Add QFileTree and bind it to QFileSystemModel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,12 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    model(new QFileSystemModel)
 {
     ui->setupUi(this);
+    model->setRootPath(QDir::currentPath());
+    ui->fileTreeView->setModel(model);
 }
 
 MainWindow::~MainWindow()
